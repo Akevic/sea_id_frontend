78457bdb35befedce5a87cea86c114235f0fcdd2
andrija <andrija.kevic@cinnamon.agency>
2022-05-31 15:42:53 +0200

    remove build warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Card, CardProps } from './components/Card'
-import { CameraBtn } from './components/CameraBtn'
-import SeaHut from './assets/seaHut.png'
+// import { Card, CardProps } from './components/Card'
+// import { CameraBtn } from './components/CameraBtn'
+// import SeaHut from './assets/seaHut.png'
 import home from './assets/home.png'
 
 export const App = () => {
   const navigate = useNavigate()
-  const [cardPropsArray] = useState<CardProps[]>([{
-    id: 1,
-    title: 'Catch Log',
-    imageUrl: SeaHut
-    },{
-      id: 2,
-      title: 'SeaHut',
-      imageUrl: SeaHut
-    },{
-      id: 3,
-      title: 'Weather',
-      imageUrl: SeaHut
-    },{
-      id: 4,
-      title: 'Calendar',
-      imageUrl: SeaHut
-    }
-  ])
+  // const [cardPropsArray] = useState<CardProps[]>([{
+  //   id: 1,
+  //   title: 'Catch Log',
+  //   imageUrl: SeaHut
+  //   },{
+  //     id: 2,
+  //     title: 'SeaHut',
+  //     imageUrl: SeaHut
+  //   },{
+  //     id: 3,
+  //     title: 'Weather',
+  //     imageUrl: SeaHut
+  //   },{
+  //     id: 4,
+  //     title: 'Calendar',
+  //     imageUrl: SeaHut
+  //   }
+  // ])
 
   const activateCamera = () => {
     navigate('/camera')
